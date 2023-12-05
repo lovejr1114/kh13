b2dce877f9602f59b62003d59b57c1282c9c8d7b
lovejr1114 <lovejr1114@naver.com>
2023-12-05 16:46:26 +0900

    test06 공부복습시간 계산 (변환기사용)

---
+++ day02/src/data/Test06공부복습.java
@@ -0,0 +1,15 @@
+package data;
+
+public class Test06공부복습 {
+	public static void main(String[] args) {
+		//어제 복습한 시간은 총 2시간 45분이고
+		//오늘 복습한 시간은 총 3시간 35분입니다.
+		//이틀동안 공부한 시간은 총 몇시간 몇분인가요?
+		
+		int yesterday = 9900;
+		int today = 12900;
+		int total = yesterday + today;
+		System.out.println(total);
+	}
+
+}
