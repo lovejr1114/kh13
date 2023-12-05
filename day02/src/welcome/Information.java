ea06baca983d7ca4637ce6a318482ed2faec1f52
lovejr1114 <lovejr1114@naver.com>
2023-12-05 12:51:58 +0900

    과정명 수정

--- day02/src/welcome/Information.java
+++ day02/src/welcome/Information.java
@@ -3,6 +3,6 @@ package welcome;
 public class Information {
 	public static void main(String[] args) {
 		System.out.println("김정은");
-		System.out.println("공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
+		System.out.println("(디지털컨버전스)공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
 	}
 }
