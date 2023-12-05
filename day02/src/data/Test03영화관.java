7fc4fe4f3c87758f4f69194c39f8c5296efe85a6
lovejr1114 <lovejr1114@naver.com>
2023-12-05 14:54:41 +0900

    test03 영화관 계산 문제 풀어보기 메모추가

--- day02/src/data/Test03영화관.java
+++ day02/src/data/Test03영화관.java
@@ -2,16 +2,20 @@ package data;
 
 public class Test03영화관 {
 	public static void main(String[] args) {
+		//입력 - 문제에서 주어진 값들을 변수에 저장
 		int adultPrice = 12000;
 		int teenPrice = 8500;
 		int adultPerson = 2;
 		int teenPerson = 2;
 		
+		//계산
 		int adultTotal = adultPrice * adultPerson;
+		//System.out.println(adultTotal);
 		int teenTotal = teenPrice * teenPerson;
+		//System.out.println(teenTotal);
 		int total = adultTotal + teenTotal;
-		System.out.println(adultTotal);
-		System.out.println(teenTotal);
+		
+		//출력 - 문제에서 요구하는 값을 화면에 출력
 		System.out.println(total);
 	}
 
