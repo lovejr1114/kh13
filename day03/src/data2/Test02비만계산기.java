b21d0f11fbdcff2d6b8e32baed89ce1f5a6b61d0
lovejr1114 <lovejr1114@naver.com>
2023-12-06 14:05:07 +0900

    Test02비만계산기 메모추가

--- day03/src/data2/Test02비만계산기.java
+++ day03/src/data2/Test02비만계산기.java
@@ -4,7 +4,7 @@ public class Test02비만계산기 {
 	public static void main(String[] args) {
 		//BMI(체질량지수) 계산 공식은 다음과 같습니다.
 		//키가 180cm, 몸무게가 80kg인 사람의 BMI 지수를 구하여 출력하세요
-		// BMI = 체중(kg) / 키 (m²)
+		// BMI = 체중(kg) / 키 (m²) -> 단위 중요
 		
 		//입력
 		double kg = 80; //int 입력도 가능 (cm)도
