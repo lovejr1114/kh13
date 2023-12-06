62ea084467fa298f7286941d90725ed6c28d6f98
lovejr1114 <lovejr1114@naver.com>
2023-12-06 13:35:36 +0900

    Test09만 나이 메모추가..

--- day03/src/data/Test09만나이.java
+++ day03/src/data/Test09만나이.java
@@ -6,7 +6,7 @@ public class Test09만나이 {
 		//2000년 1월 1일생의 만나이를 구하여 출력하라
 		//단, 생년월일은 20231206처럼 8자리 숫자형태로 제공됩니다.
 		//단, 일은 고려하지 않고 월까지만 고려합니다.
-		//287개월 23살
+		//287개월 23살이 나와야함..
 		
 		//입력 
 		int current = 20231206;
@@ -20,8 +20,6 @@ public class Test09만나이 {
 		
 		int year = today - age; //23
 		//System.out.println(year);
-		int a = year *12;
-		//System.out.println(a);
 		
 		int month = current / 100 % 100; //12
 		//System.out.println(month);
@@ -40,6 +38,7 @@ public class Test09만나이 {
 		int birthYear = birth / 10000; //2000
 		int birthMonth = birth % 10000 / 100; //1
 		
+		
 		int now = currentYear * 12 + currentMonth;
 		int before = birthYear * 12 + birthMonth;
 		int gap =  now - before; //개월 수
@@ -49,6 +48,14 @@ public class Test09만나이 {
 		
 		//출력
 		System.out.println(age1);
+		
+		//20231206 중, 10000으로 나눠 네 자리수 (출생연도)를 구한다.
+		//그리고 월 단위인  12월을 구하기 위해 20231206 몫 100 나머지 100
+		//(또는 나머지 100 나누기 100해보고 안나오면 월 나올 때까지 입력해보기)
+		//일 수도 월 단위 계산법과 똑같이 계산해준다.
+		//구한 값을 낮은 단위인 개월 수로 바꿔준다. (계산한 연도 * 12(변하지않는상수) + 일 수)
+		//현재 시간에서 출생한날짜를 빼준다.
+		//위 나온 값에서 12개월을 나눠준다. 그럼 만 나이가 나온다.
 		
 
 	}
