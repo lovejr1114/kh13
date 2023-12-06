f458c33bf155b45457af1c2f48edd596ff635046
lovejr1114 <lovejr1114@naver.com>
2023-12-06 12:37:40 +0900

    메모 오타 수정

--- day03/src/data/Test08나눗셈.java
+++ day03/src/data/Test08나눗셈.java
@@ -2,7 +2,7 @@ package data;
 
 public class Test08나눗셈 {
 	public static void main(String[] args) {
-		//위 숙자의 10의자리, 1의자리 숫자를 각각 출력
+		//위 숫자의 10의자리, 1의자리 숫자를 각각 출력
 		int number = 75;
 				
 		System.out.println(number / 10);
@@ -11,7 +11,7 @@ public class Test08나눗셈 {
 		//숫자의 100의 자리, 10의 자리, 1의자리를 각각 출력
 		int number2 = 139;
 		
-		System.out.println(number2 / 100); //- /10/10도 가능
+		System.out.println(number2 /10 /10); //- /100도 가능
 		System.out.println(number2 /10 % 10);
 		System.out.println(number2 % 10);
 		//중요한건 내가 원하는 숫자로 잘라낼 수 있느냐
