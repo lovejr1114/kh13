15d546550283e95679bc73281ca2cbdd36742a85
lovejr1114 <lovejr1114@naver.com>
2023-12-06 15:04:07 +0900

    Test03급여계산기 변환연산

--- day03/src/data2/Test03급여계산기.java
+++ day03/src/data2/Test03급여계산기.java
@@ -14,17 +14,20 @@ public class Test03급여계산기 {
 		int salary = 3000000;
 		
 		//계산
-		double pension = salary * 0.045;//국민연금
-		double health = salary * 0.0709;//건보료
-		double care = health * 0.1281;//장기요양
-		double emp = salary * 0.009;//고용보험
-		double actual = salary - pension - health - care - emp;
+		//-변환 연산을 사용하여 double을 int로 바꿔서 저장 (당연히 소수점은 날라간다)
+		int pension = (int) (salary * 0.045);//국민연금, 괄호 안 기능은 오른쪽 역할 바꿔줌
+		//int pension = salary * 45 / 1000; //이렇게 하면 변환이 필요 없다.
+		int health = (int) (salary * 0.0709);//건보료
+		int care = (int) (health * 0.1281);//장기요양
+		int emp = (int) (salary * 0.009);//고용보험
+		int actual = salary - pension - health - care - emp;
 
+		
 		
 		//출력
 		System.out.println(actual); //실수령액
 		System.out.println(pension);
-		System.out.println(health);
+		System.out.println(health);	
 		System.out.println(care);
 		System.out.println(emp);
 
