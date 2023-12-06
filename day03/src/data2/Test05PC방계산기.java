6fa8c6f6e8a08ef145e0977ce612a1da31ffa040
lovejr1114 <lovejr1114@naver.com>
2023-12-06 16:17:35 +0900

    Test05PC방요금계산 선생님풀이2

--- day03/src/data2/Test05PC방계산기.java
+++ day03/src/data2/Test05PC방계산기.java
@@ -3,12 +3,14 @@ package data2;
 public class Test05PC방계산기 {
 	public static void main(String[] args) {
 		//1차로 이용시간 구하기
+		//2차로 요금 구하기
 		
 		//입력
 		//int inHour = 12, inMinute =20;
 		int in = 1220;
 		int out = 1530;
 		
+		int unitPrice = 1000;
 		
 		//계산
 		int inHour = in /100; //들어온 시각
@@ -23,10 +25,19 @@ public class Test05PC방계산기 {
 		int hour = time / 60;
 		int minute = time % 60;
 		
+		//분당 요금
+		//double minutePrice = unitPrice / 60.0;
+		//double minutePrice = unitPrice / 60d;
+		double minutePrice = (double)unitPrice / 60;
+		
+		int result = (int)(time * minutePrice);
+		
+		
 		
 		//출력
 		System.out.println(hour);
 		System.out.println(minute);
+		System.out.println(result);
 		
 		
 		
