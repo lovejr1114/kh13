9d1ec1484a3c832eadbec5839fd9538d32f53927
lovejr1114 <lovejr1114@naver.com>
2023-12-06 17:16:19 +0900

    자바의 문자 다루기

---
+++ day03/src/data3/Test01문자.java
@@ -0,0 +1,21 @@
+package data3;
+
+public class Test01문자 {
+	public static void main(String[] args) {
+		//자바의 문자
+		//- 자바는 유니코드셋을 표현 할 수 있다.
+		//- char 형태로 표현 (한 글자만 가능)
+		//- 글자이지만 숫자로 변환이 가능
+		
+		char a ='가';
+		System.out.println(a);
+		
+		int b = a;
+		System.out.println(b);
+		
+		char c = 55203;
+		System.out.println(c);
+		
+	}
+
+}
