d203f9d6fd8d51513939c97d941bd2735e3fa846
lovejr1114 <lovejr1114@naver.com>
2023-12-07 10:10:26 +0900

    페이지번호계산 선생님풀이

--- day03/src/data2/Test06페이지번호계산기.java
+++ day03/src/data2/Test06페이지번호계산기.java
@@ -13,10 +13,26 @@ public class Test06페이지번호계산기 {
 		//System.out.println(first);
 		//System.out.println(last);
 		
+		
 		//출력
-		System.out.println(page + "페이지의 첫 페이지는 " + first + "페이지이고, 마지막 페이지는 " + last + "페이지 입니다.");
+		//System.out.println(page + "페이지의 첫 페이지는 " + first + "페이지이고, 마지막 페이지는 " + last + "페이지 입니다.");
 
 		
+		//선생님풀이
+		//입력
+		int page1 = 4;
+		int size = 10; //보여줄 번호의 개수
+		
+		//계산
+		int begin = (page1 - 1) / size * size + 1;
+		//int end = (page1 - 1) / size * size + 10;
+		int end = begin + (size - 1);
+		System.out.println(begin);
+		System.out.println(end);
+		
+		//출력
+		//System.out.println("시작번호 : " +begin);
+		//System.out.println("종료번호 : " + end);
 	}
 
 }
