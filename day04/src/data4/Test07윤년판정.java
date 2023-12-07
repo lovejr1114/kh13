d2c9133465e13cc696c5a5a3d4f1a2773cf34cd8
lovejr1114 <lovejr1114@naver.com>
2023-12-07 15:30:00 +0900

    윤년판정 선생님 풀이

--- day04/src/data4/Test07윤년판정.java
+++ day04/src/data4/Test07윤년판정.java
@@ -10,17 +10,33 @@ public class Test07윤년판정 {
 		//2100년에 윤년인지 아닌지 판정 출력
 		
 		//입력
-		int obYear = 2100;
+		//int obYear = 2100;
 		
 		//계산
-		boolean multi = obYear % 4 == 0 ;
-		System.out.println(multi);
-		boolean  four = obYear % 400 == 0 && obYear % 100 == 0;
-		System.out.println(four);
+		//boolean multi = obYear % 4 == 0 ;
+		//System.out.println(multi);
+		//boolean  four = obYear % 400 == 0 && obYear % 100 == 0;
+		//System.out.println(four);
 		//boolean = 2100년은 윤년인가
 		//boolean result = 
 		
+		//선생님 풀이
+		//입력
+		int year = 2100;
+		
+		//계산
+		//boolean condition1 = year가 4의 배수?
+		boolean condition1 = year % 4 ==0;
+		//boolean condition2 = year가 100의 배수가 아닌가?
+		boolean condition2 = year % 100 != 0;
+		//boolean condition3 = year가 400의 배수?
+		boolean condition3 = year % 400 == 0;
+		System.out.println(condition3);
+		
+		boolean leap = condition3 || (condition1 && condition2);
+		
 		//출력
+		System.out.println("윤년인가요? " + leap);
 	}
 
 }
