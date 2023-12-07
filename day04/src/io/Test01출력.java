dbf718e36527aef0fbeec45d3257845a6bda8b34
lovejr1114 <lovejr1114@naver.com>
2023-12-07 16:04:51 +0900

    출력

---
+++ day04/src/io/Test01출력.java
@@ -0,0 +1,17 @@
+package io;
+
+public class Test01출력 {
+	public static void main(String[] args) {
+		//표준 출력 통로 (System.out)
+		//- 시스템의 기본 출력 장치를 이용할 수 있도록 하는 도구
+		//- println 명령은 글자 출력 후 줄바꿈(개행) 수행 (ln은 line의 줄임)
+		//- print 명령은 글자만 출력
+		
+		System.out.println("Hello!");
+		System.out.print("JA");
+		System.out.print("BA");
+		
+		
+	}
+
+}
