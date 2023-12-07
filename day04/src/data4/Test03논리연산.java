67d2cb9f752edbe392b6e13748c5f3743ee7ee31
lovejr1114 <lovejr1114@naver.com>
2023-12-07 12:45:36 +0900

    논리연산2

--- day04/src/data4/Test03논리연산.java
+++ day04/src/data4/Test03논리연산.java
@@ -9,10 +9,15 @@ public class Test03논리연산 {
 		//예) 19살은 청소년인가요?
 		//- 청소년은 14살부터 19살까지 입니다.
 		
-		int age = 19;
+		int age = 5;
 		//boolean teen = 14 <= age <= 19; <-잘못된 식, 논리연산끼리 묶어줘야함 && , ||
-		boolean teen = 14 <= age&&age<=19;
+		boolean teen = 14 <= age && age<=19;
 		System.out.println("청소년인가요? " + teen);
+		
+		//- 무임승차 대상은 65세 이상 또는 8세 미만입니다.
+		
+		boolean free = age >= 65 || age < 8;
+		System.out.println("무임승차 대상인가요? " + free);
 	}
 
 }
