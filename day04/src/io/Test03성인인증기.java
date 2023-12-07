3797b5330638d2dab04fbc3c20b6715afff07ab4
lovejr1114 <lovejr1114@naver.com>
2023-12-07 16:26:13 +0900

    성인인증기 스캐너 예제

---
+++ day04/src/io/Test03성인인증기.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.util.Scanner;
+
+public class Test03성인인증기 {
+	public static void main(String[] args) {
+		//입력
+		Scanner sc = new Scanner(System.in);
+		int birth = sc.nextInt();		
+		
+		//계산
+		int age = 2023 - birth + 1;
+		boolean adult = age >= 20;
+		
+		//출력
+		System.out.println("나이 = " + age);
+		System.out.println("성인 = " + adult);
+		
+	}
+
+}
