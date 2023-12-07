66db566e9dfc61ebb99f42142acfc8d42d85bb09
lovejr1114 <lovejr1114@naver.com>
2023-12-07 17:03:29 +0900

    평균계산기 선생님풀이

--- day04/src/io/Test04평균계산기.java
+++ day04/src/io/Test04평균계산기.java
@@ -5,14 +5,17 @@ import java.util.Scanner;
 public class Test04평균계산기 {
 	public static void main(String[] args) {
 		//입력
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //스캐너는 하나만 사용해도 된다.
 		int korean = sc.nextInt();
 		int english = sc.nextInt();
 		int math = sc.nextInt();
 		
 		//계산
 		int result = korean + english + math;
-		int average = result / 3;
+		//int average = result / 3;
+		double average = result / 3.0; //-> 평균은 소수점이 나와야하기 때문에 더블로 입력해준다.
+		//double average = result / 3d;
+		//double average = (double)result / 3;
 		
 		//출력
 		//System.out.println("국어점수 = " + korean);
