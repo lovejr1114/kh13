6bf4f7dd43dabbfc788886d659e741d3fc3aeb45
lovejr1114 <lovejr1114@naver.com>
2023-12-07 14:08:08 +0900

    건강검진 혼자한거

---
+++ day04/src/data4/Test06건강검진.java
@@ -0,0 +1,17 @@
+package data4;
+
+public class Test06건강검진 {
+	public static void main(String[] args) {
+		
+		int ob = 1985;
+		int thisY = 2023;
+		
+		boolean thirty = thisY - ob + 1 >= 30;
+		//System.out.println(thirty);
+		boolean even = thisY + 1 % 2 == 0;
+		System.out.println(even);
+		//30살 이상인가 / 짝수.짝수 / 홀수,홀수
+;
+	}
+
+}
