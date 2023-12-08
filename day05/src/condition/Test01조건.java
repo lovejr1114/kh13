2dc460ef65f5f535345e52d6c1949362619fd333
lovejr1114 <lovejr1114@naver.com>
2023-12-08 10:24:26 +0900

    조건 홀짝 두 번 물어본거

---
+++ day05/src/condition/Test01조건.java
@@ -0,0 +1,25 @@
+package condition;
+
+public class Test01조건 {
+	public static void main(String[] args) {
+		//홀수 짝수 판정 프로그램
+		//- 주어진 값이 홀수라면 "홀수입니다" 출력
+		//- 주어진 값이 짝수라면 "짝수입니다" 출력
+
+		//입력
+		int number = 123;
+		
+		//계산
+		boolean odd = number % 2 != 0; 
+		boolean even = number % 2 == 0;
+		
+		//출력
+		if(odd == true) {
+			System.out.println("홀수입니다.");  //odd가 true일 때 나옴.
+		}
+		if(even == true) {
+			System.out.println("짝수입니다.");
+		}
+	}
+
+}
