ccf51d4049c826bb257a1f1390f9f19b39be1e53
lovejr1114 <lovejr1114@naver.com>
2023-12-08 10:07:02 +0900

    분식점계산기 선생님코드

---
+++ day04/src/io/Test06분식점2.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.util.Scanner;
+
+public class Test06분식점2 {
+	public static void main(String[] args) {
+		
+		//입력
+		int ddeok = 3000;
+		int fried = 500;
+		int sundae = 2000;
+		int rate = 10;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("주문 할 떡볶이 수량 : ");
+		int ddeokCount = sc.nextInt();
+		System.out.print("주문 할 튀김 수량 : ");
+		int friedCount = sc.nextInt();
+		System.out.print("주문 할 순대 수량 : ");
+		int sundaeCount = sc.nextInt();
+		
+		//계산
+		int ddeokTotal = ddeok * ddeokCount;
+		int friedTotal = fried * friedCount;
+	    int sundaeTotal = sundae * sundaeCount;
+	    
+		int total = ddeokTotal + friedTotal + sundaeTotal;
+		
+		//int discount = (int) (total * 0.1); //이런 식은 괄호 때문에 위험할수있음
+		int discount = total * rate /100; //나누기를 먼저하면 날라갈 수 있기 때문에 곱 먼저
+		int result = total - discount;
+		
+		//출력
+		System.out.println("총 금액 : " + total + "원");
+		System.out.print("할인 금액 : " + discount + "원");
+		System.out.println(" (" + rate + "% 할인)");
+		System.out.println("결제 금액은 " + result + "원입니다.");
+		
+	}
+
+}
