e00836d193ac4241a91ae4699f6f6660e17ed079
lovejr1114 <lovejr1114@naver.com>
2023-12-08 10:31:56 +0900

    조건- 계산식 하나 뺌

---
+++ day05/src/condition/Test01조건2.java
@@ -0,0 +1,24 @@
+package condition;
+
+public class Test01조건2 {
+	public static void main(String[] args) {
+		//홀수 짝수 판정 프로그램
+		//- 주어진 값이 홀수라면 "홀수입니다" 출력
+		//- 주어진 값이 짝수라면 "짝수입니다" 출력
+
+		//입력
+		int number = 123;
+		
+		//계산
+		boolean odd = number % 2 != 0; 
+		
+		//출력
+		if(odd == true) {
+			System.out.println("홀수입니다.");  //odd가 true일 때 나옴.
+		}
+		if(odd == false) { //부정적인 표현보단 알아보기 쉬운 코드로 ex) != 같이..
+			System.out.println("짝수입니다.");
+		}
+	}
+
+}
