64e2d03b94340fb90f84319cb75aadf1f26bb75d
lovejr1114 <lovejr1114@naver.com>
2023-12-11 17:03:39 +0900

    감소반복 메모 추가

--- day06/src/loop/Test08감소하는반복.java
+++ day06/src/loop/Test08감소하는반복.java
@@ -3,6 +3,9 @@ package loop;
 public class Test08감소하는반복 {
 	public static void main(String[] args) {
 		//(Q) 10부터 0까지 줄어드는 카운트다운 출력
+		//범위 조심
+		//증가가 아닌 감소 처리 해야함
+		
 		for(int i=10; i >= 0; i--) { //조심해야할 것은 조건식이 더 커야 한다.
 			System.out.println(i);
 		}
