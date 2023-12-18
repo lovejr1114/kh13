083de85aedbf2f2297ca94ee6d53ab831029638e
lovejr1114 <lovejr1114@naver.com>
2023-12-18 16:46:02 +0900

    카페 메뉴 할인 메소드쪽 불린,이프문 선생님 코드

--- day11/src/oop/method4/Test01메소드사용.java
+++ day11/src/oop/method4/Test01메소드사용.java
@@ -3,19 +3,19 @@ package oop.method4;
 public class Test01메소드사용 {
 	public static void main(String[] args) {
 		
-		Cafe m1 = new Cafe();
-		Cafe m2 = new Cafe();
-		Cafe m3 = new Cafe();
-		Cafe m4 = new Cafe();
+		Cafe a = new Cafe();
+		Cafe b = new Cafe();
+		Cafe c = new Cafe();
+		Cafe d = new Cafe();
 		
-		m1.data("아메리카노","음료",2500,"(행사중)");
-		m2.data("모카라떼","음료",3500,"-");
-		m3.data("치즈케이크", "디저트", 5000,"(행사중)");
-		m4.data("마카롱", "디저트",3000,"-");
+		a.data("아메리카노", "음료", 2500, true);
+		b.data("모카라떼", "음료", 3500, false);
+		c.data("치즈케이크", "디저트", 5000, true);
+		d.data("마카롱", "디저트", 3000, false);
 		
-		m1.information();
-		m2.information();
-		m3.information();
-		m4.information();
+		a.information();
+		b.information();
+		c.information();
+		d.information();
 	}
 }
