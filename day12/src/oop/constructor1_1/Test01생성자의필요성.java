aab04d2d5836d8915f7e0e37bf558aeb97997403
lovejr1114 <lovejr1114@naver.com>
2023-12-19 18:45:09 +0900

    ?

--- day12/src/oop/constructor1_1/Test01생성자의필요성.java
+++ day12/src/oop/constructor1_1/Test01생성자의필요성.java
@@ -2,7 +2,7 @@ package oop.constructor1_1;
 
 public class Test01생성자의필요성 {
 	public static void main(String[] args) {
-		Student a = new Student("피카츄",50);		
+		Student a = new Student("피카츄", 50);		
 		a.information();
 		
 		Student b = new Student("라이츄");
