89f022e929df0a853173f776953e8f4de254022a
lovejr1114 <lovejr1114@naver.com>
2023-12-22 14:26:02 +0900

    인터페이스와 다중상속 개념

---
+++ day15/src/oop/multi1/Comedian.java
@@ -0,0 +1,5 @@
+package oop.multi1;
+
+public interface Comedian {
+	/*public abstract는 자동으로 붙는다 인터페이스에서는.*/void gag();
+}
---
+++ day15/src/oop/multi1/Person.java
@@ -0,0 +1,16 @@
+package oop.multi1;
+
+//인터페이스는 클래스와 상속키워드가 다르다다
+public class Person /*extends 클래스(핵심상속 super)*/ implements Singer, Comedian{ //(부가상속)
+
+	@Override
+	public void gag() {
+		
+	}
+
+	@Override
+	public void sing() {
+		
+	}
+	
+}
