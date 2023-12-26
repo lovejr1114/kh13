bc2093c563bd08bd0a54b9fac97e1f7c58d49cb3
lovejr1114 <lovejr1114@naver.com>
2023-12-26 15:12:52 +0900

    문제 내용 추가

--- day16/src/api/lang/string/Test01객체생성문제.java
+++ day16/src/api/lang/string/Test01객체생성문제.java
@@ -2,6 +2,24 @@ package api.lang.string;
 
 public class Test01객체생성문제 {
 	public static void main(String[] args) {
+		/*
+		 api.lang.string 패키지
+java.lang 패키지에 있는 StringBuffer 클래스의 문서를 읽고 다음 요구사항에 대한 코드를 작성하세요
+(Test01객체생성문제)
+
+객체 생성
+a라는 이름으로 비어 있는 객체 생성
+b라는 이름으로 hello라는 글자를 저장한 객체 생성
+a와 b의 요약 정보를 출력
+a와 b의 보관중인 글자수를 각각 출력
+a와 b에 각각 java라는 글자를 추가
+a와 b의 요약 정보와 보관 중인 글자 수를 다시 출력
+*/
+		
+		
+		
+		
+		
 		//객체 생성
 		StringBuffer a = new StringBuffer();
 		StringBuffer b = new StringBuffer("hello");
