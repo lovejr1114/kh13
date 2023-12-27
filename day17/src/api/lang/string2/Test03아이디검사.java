f0257d94f35cb72ce1c656fb2ab3724612d6aa58
lovejr1114 <lovejr1114@naver.com>
2023-12-27 15:06:23 +0900

    (플젝때중요할듯) 아이디검사하기(네이버양식처럼함)

---
+++ day17/src/api/lang/string2/Test03아이디검사.java
@@ -0,0 +1,22 @@
+package api.lang.string2;
+
+import java.util.Scanner;
+
+public class Test03아이디검사 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("아이디 입력 : ");
+		String id = sc.nextLine();
+		
+		String regex = "^[a-z0-9][a-z0-9-_]{4,19}$";
+//		String regex = "^[a-z0-9]{1}[a-z0-9-_]{4,19}$"; //써도되지만 권장하지는 않는다. 하지만 헷갈리면 적어도됨.
+		 
+		if(id.matches(regex)) {
+			System.out.println("멋진 아이디입니다!");
+		}
+		else {
+			System.out.println("5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.");
+		}
+	}
+}
