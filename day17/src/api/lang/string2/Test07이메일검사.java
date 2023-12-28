fce83afa16897361de9c13f4207ddee7635d7a97
lovejr1114 <lovejr1114@naver.com>
2023-12-28 09:55:53 +0900

    이메일검사주석지움

---
+++ day17/src/api/lang/string2/Test07이메일검사.java
@@ -0,0 +1,36 @@
+package api.lang.string2;
+
+import java.util.Scanner;
+
+public class Test07이메일검사 {
+	public static void main(String[] args) {
+		/*사용자에게 입력받은 이메일이 유효한지 검사
+		요구사항
+		반드시 @가 한 개 있어야 한다
+		@ 앞부분과 뒷부분에 글자가 있어야 한다
+		@ 앞부분은 아이디와 동일한 형식으로 검사
+		네이버 아이디 검사 코드 참조
+		(5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.)
+		@ 뒷부분은 기관 주소가 들어온다
+		기관 명칭이 우선 나오고 확장자가 등장한다
+		(ex) 삼성이면 ooooo@samsung.com
+		기관 명칭은 소문자 3글자 ~ 20글자 사이로 설정가능
+		확장자는 다음 중 하나만 가능
+		.co.kr
+		.com
+		.net
+		.org
+		.dev
+		*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("이메일을 입력해주세요. \n: ");
+		String email = sc.nextLine();
+		
+		String regex = "^[a-z0-9][a-z0-9-_]{4,19}@{1}[a-z]{3,20}\\.(co\\.kr|com|net|org|dev)$";
+
+		
+		System.out.println(email.matches(regex));
+	}
+}
