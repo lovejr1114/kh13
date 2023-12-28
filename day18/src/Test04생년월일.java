a426e65782548f55af07b1848c0cb7fbd10c3924
lovejr1114 <lovejr1114@naver.com>
2023-12-28 16:17:02 +0900

    생년월일문제 2는 선생님 풀이(substring 사용) 3은 split으로 사용하여 푼 것

---
+++ day18/src/Test04생년월일.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Test04생년월일 {
+	public static void main(String[] args) {
+		/*사용자에게 생년월일을 YYYY-MM-DD 형태로 입력받아서
+		연도, 월, 일을 각각 분리한 뒤 숫자로 변환해서 출력
+		생년월일 입력 : 2023-12-28 (String)
+		```int
+		출생년도 : 2023년
+		출생월 : 12월
+		출생일 : 28일
+		```
+		문제가 발생하는 상황을 try~catch 구문을 이용해서 처리한 뒤 메세지 작성*/
+		
+		
+		try {			
+			Scanner sc = new Scanner(System.in);		
+			System.out.print("생년월일 입력 (YYYY-MM-DD) \n:");
+			String birthDate = sc.nextLine();
+			
+			int year = Integer.parseInt(birthDate.substring(0, 4));
+			int month = Integer.parseInt(birthDate.substring(5,7));
+			int day = Integer.parseInt(birthDate.substring(8,10));
+			
+			System.out.println("출생년도 : " + year +"년");
+			System.out.println("출생월 : "+month+"월");
+			System.out.println("출생일 : "+day+"일");
+		}
+		catch(Exception e){
+			System.err.println("입력이 잘못되었습니다.");
+		}
+	}
+}
---
+++ day18/src/Test04생년월일2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Test04생년월일2 {
+	public static void main(String[] args) {
+		/*사용자에게 생년월일을 YYYY-MM-DD 형태로 입력받아서
+		연도, 월, 일을 각각 분리한 뒤 숫자로 변환해서 출력
+		생년월일 입력 : 2023-12-28 (String)
+		```int
+		출생년도 : 2023년
+		출생월 : 12월
+		출생일 : 28일
+		```
+		문제가 발생하는 상황을 try~catch 구문을 이용해서 처리한 뒤 메세지 작성*/
+		
+		try {			
+			Scanner sc = new Scanner(System.in);		
+			System.out.print("생년월일 입력 (YYYY-MM-DD) \n:");
+			String birthDate = sc.nextLine();
+			
+			String yearStr = birthDate.substring(0, 4);
+			String monthStr = birthDate.substring(5,7);
+			String dayStr = birthDate.substring(8);
+			
+			int year = Integer.parseInt(yearStr);
+			int month = Integer.parseInt(monthStr);
+			int day = Integer.parseInt(dayStr);
+			
+			System.out.println("출생년도 : " + year +"년");
+			System.out.println("출생월 : "+month+"월");
+			System.out.println("출생일 : "+day+"일");
+		}
+		catch(Exception e){
+			System.err.println("입력이 잘못되었습니다.");
+		}
+	}
+}
