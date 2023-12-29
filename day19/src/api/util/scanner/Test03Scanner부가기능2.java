e20028b1d388e1740b919ac7d095ba000f3dcd0a
lovejr1114 <lovejr1114@naver.com>
2023-12-29 17:28:38 +0900

    Scanner클래스 useDelimiter와 findInLine

---
+++ day19/src/api/util/scanner/Test03Scanner부가기능2.java
@@ -0,0 +1,14 @@
+package api.util.scanner;
+
+import java.util.Scanner;
+
+public class Test03Scanner부가기능2 {
+	public static void main(String[] args) {
+		String colors = "빨강+주황/노랑-초록=파랑*남색_보라";
+		
+		Scanner sc = new Scanner(colors);
+		String find = sc.findInLine("[가-힣]+");
+		System.out.println("find = "+find);
+		sc.close();
+	}
+}
