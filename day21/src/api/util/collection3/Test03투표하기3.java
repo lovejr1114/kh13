e4486726ae4d6eac8495368ff5df3b1521da9b7c
lovejr1114 <lovejr1114@naver.com>
2024-01-03 17:54:17 +0900

    투표하기 문제 선생님 코드 복붙한거 다시 풀어볼것

--- day21/src/api/util/collection3/Test03투표하기3.java
+++ day21/src/api/util/collection3/Test03투표하기3.java
@@ -29,38 +29,38 @@ public class Test03투표하기3 {
 //		{피카츄=3, 라이츄=2}
 		
 		//저장소 생성
-		Map<String, Integer> votes = new HashMap<>();
+				Map<String, Integer> votes = new HashMap<>();
 
-		//더미 데이터 추가
-		//votes.put("피카츄", 25);
-		//votes.put("라이츄", 17);
+				//더미 데이터 추가
+				//votes.put("피카츄", 25);
+				//votes.put("라이츄", 17);
 
-		Scanner sc = new Scanner(System.in);
+				Scanner sc = new Scanner(System.in);
 
-		while(true) {
-			//사용자 입력
-			System.out.print("투표할 대상 이름 : ");
-			String input = sc.nextLine();
-			if(input.equals("종료")) {
-				break;
-			}
+				while(true) {
+					//사용자 입력
+					System.out.print("투표할 대상 이름 : ");
+					String input = sc.nextLine();
+					if(input.equals("종료")) {
+						break;
+					}
 
-			//판정
-			Integer count = votes.get(input);//득표수 추출(없으면 null)
-			if(count == null) {
-				count = 1;
-			}
-			else {
-				count = count + 1;
-			}
-			votes.put(input, count);
-			System.out.println("["+input+"] 투표 완료! 득표수 "+count);
+					//판정
+					Integer count = votes.get(input);//득표수 추출(없으면 null)
+					if(count == null) {
+						count = 1;
+					}
+					else {
+						count = count + 1;
+					}
+					votes.put(input, count);
+					System.out.println("["+input+"] 투표 완료! 득표수 "+count);
 
-			}
+				}
 
-		sc.close();
+				sc.close();
 
-		//출력
-		System.out.println(votes);
+				//출력
+				System.out.println(votes);
 	}
 }
