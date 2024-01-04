2044cd7ac272b096ee75b6ff84f40bf9c35a508e
lovejr1114 <152838214+lovejr1114@users.noreply.github.com>
2024-01-04 17:54:02 +0900

    Update create02.테이블제약조건.sql

--- database/create02.테이블제약조건.sql
+++ database/create02.테이블제약조건.sql
@@ -42,3 +42,27 @@ INSERT INTO class(class_course,class_time,class_price,class_type)
 	values('웹 개발자 단기완성',120,1200000,'오프라인');
 
 SELECT * FROM class;
+
+
+
+-- 휴대폰 판매 정보 테이블
+create table phone(
+phone_name varchar2(60) not null,
+phone_telecom char(2) not null check(phone_telecom in ('SK', 'KT', 'LG')),
+phone_price number not null check(phone_price >= 0),
+phone_contract number(2) check(phone_contract in (0, 24, 36)),
+-- 이름+통신사를 합쳐서 같은 경우는 없다
+unique(phone_name, phone_telecom)
+);
+drop table phone;
+
+insert into phone(phone_name, phone_telecom, phone_price, phone_contract)
+values('갤럭시Fold4', 'SK', 1800000, null);
+insert into phone(phone_name, phone_telecom, phone_price)
+values('갤럭시Fold4', 'SK', 1800000);
+insert into phone(phone_name, phone_telecom, phone_price, phone_contract)
+values('갤럭시Fold4', 'KT', 1750000, 24);
+insert into phone(phone_name, phone_telecom, phone_price, phone_contract)
+values('아이폰15', 'LG', 2000000, 36);
+insert into phone(phone_name, phone_telecom, phone_price, phone_contract)
+values('아이폰15', 'SK', 1990000, null);
