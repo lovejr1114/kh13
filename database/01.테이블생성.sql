d66c248a4ff1c7138ee4ea87c5dc992b498a3d0b
lovejr1114 <152838214+lovejr1114@users.noreply.github.com>
2024-01-04 15:16:14 +0900

    Update 01.테이블생성.sql

--- database/01.테이블생성.sql
+++ database/01.테이블생성.sql
@@ -30,3 +30,22 @@ bronze number(3)
 );
 
 drop table olympic;
+
+
+--통신사 요금제 테이블 생성 및 데이터 추가 코드
+CREATE TABLE telecom(
+agency varchar2(45),
+goods varchar2(50),
+monthly1 NUMBER,
+GB_data number(3),
+calling number(4),
+sms number(4)
+);
+
+DROP TABLE telecom;
+
+INSERT INTO telecom(agency,goods,monthly1,GB_data,calling,sms) values('SK','5G언택트52',52000,200,1000,2000);
+INSERT INTO telecom(agency,goods,monthly1,GB_data,calling,sms) values('KT','5G세이브',54000,100,900,1500);
+INSERT INTO telecom(agency,goods,monthly1,GB_data,calling,sms) values('LG','5G시그니처',130000,500,2000,2500);
+
+SELECT *FROM telecom;
