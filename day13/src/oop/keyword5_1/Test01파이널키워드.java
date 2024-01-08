1fae84bb071000d107ea03d49744f9e63d2e61b1
lovejr1114 <lovejr1114@naver.com>
2024-01-08 15:00:30 +0900

    아이디 주석처리

--- day13/src/oop/keyword5_1/Test01파이널키워드.java
+++ day13/src/oop/keyword5_1/Test01파이널키워드.java
@@ -4,7 +4,7 @@ public class Test01파이널키워드 {
 	public static void main(String[] args) {
 		Member m = new Member("lovejr1114", "1234");
 		m.setPassword("4567");
-		m.id = "kje1114";
+//		m.id = "kje1114";
 		
 		m.information();
 	}
