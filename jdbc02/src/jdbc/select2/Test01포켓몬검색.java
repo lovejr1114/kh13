fff0d03b25a6fc6bb909888f01ea648e11e60eae
lovejr1114 <lovejr1114@naver.com>
2024-01-09 18:05:14 +0900

    포켓몬검색 방법 2가지

---
+++ jdbc02/src/jdbc/select2/Test01포켓몬검색.java
@@ -0,0 +1,35 @@
+package jdbc.select2;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jdbc.select.PocketmonDto;
+import jdbc.select.PocketmonMapper;
+import jdbc.util.jdbcHelper;
+
+public class Test01포켓몬검색 {
+	public static void main(String[] args) {
+		//유사검색
+		//- 시작 검색 or 포함검색
+		//- 상황에 맞게 like 연산자 또는 instr 함수를 사용
+		
+		//데이터 준비
+		String keyword = "피카";
+		
+//		keyword += "%"; // Java에서 처리하는 경우
+		
+		//처리
+		JdbcTemplate jdbcTemplate = jdbcHelper.getJdbcTemplate();
+		String sql = "select * from pocketmon where pocketmon_name like ? || '%'"; //DB 방법
+		Object[] data = {keyword};
+		PocketmonMapper mapper = new PocketmonMapper();
+		
+		List<PocketmonDto> list = jdbcTemplate.query(sql, mapper, data);
+		
+		System.out.println("조회 결과 : "+list.size()+"개");
+		for(PocketmonDto dto:list) {
+			System.out.println(dto.getPocketmonName());
+		}
+	}
+}
