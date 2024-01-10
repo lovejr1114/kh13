f327ef0681b9d1feea3e7be0d0181bef39e2aaa8
lovejr1114 <lovejr1114@naver.com>
2024-01-10 12:52:56 +0900

    도우미 클래스 복붙해서 준비함

---
+++ jdbc03/src/jdbc/dto/PocketmonDto.java
@@ -0,0 +1,36 @@
+package jdbc.dto;
+
+//DTO
+//- Data Transfer Object 의 줄임말
+//- 한글로 말하면 "변환용 객체"
+//- 선생님은 이걸 "포장 상자"라고 부른다
+//- 테이블당 1개씩 테이블과 똑같이 생성
+public class PocketmonDto {
+	//필드 - DB의 컬럼명을 작성
+	private int pocketmonNo;
+	private String pocketmonName;
+	private String pocketmonType;
+	//필드를 만들고 추가로 setter / getter / 기본생성자(매개변수가 하나도 없는 생성자)를 구현
+	public int getPocketmonNo() {
+		return pocketmonNo;
+	}
+	public void setPocketmonNo(int pocketmonNo) {
+		this.pocketmonNo = pocketmonNo;
+	}
+	public String getPocketmonName() {
+		return pocketmonName;
+	}
+	public void setPocketmonName(String pocketmonName) {
+		this.pocketmonName = pocketmonName;
+	}
+	public String getPocketmonType() {
+		return pocketmonType;
+	}
+	public void setPocketmonType(String pocketmonType) {
+		this.pocketmonType = pocketmonType;
+	}
+	public PocketmonDto() {
+		super();
+	}
+	
+}
---
+++ jdbc03/src/jdbc/mapper/PocketmonMapper.java
@@ -0,0 +1,25 @@
+package jdbc.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import jdbc.dto.PocketmonDto;
+
+//Mapper 파일
+//- 데이터베이스의 조회 결과 (ResultSet)를 변환객체(PocketmonDto)로 바꿔주는 역할
+//- 상속을 받아서 특정 메소드를 구현해야함
+public class PocketmonMapper implements RowMapper<PocketmonDto>{
+
+	@Override
+	public PocketmonDto mapRow(ResultSet rs, int idx) throws SQLException {
+		//rs에 담긴 정보를 PocketmonDto로 옮겨서 반환하는 코드 작성
+		PocketmonDto dto = new PocketmonDto(); //비어있는 객체 생성
+		dto.setPocketmonNo(rs.getInt("pocketmon_no"));//rs에 들어있는 pocketmon_no 항목
+		dto.setPocketmonName(rs.getString("pocketmon_name")); //rs에 들어있는 pocketmon_name 항목
+		dto.setPocketmonType(rs.getString("pocketmon_type")); //rs에 들어있는 pocketmon_type 항목
+		return dto;
+	}
+
+}
