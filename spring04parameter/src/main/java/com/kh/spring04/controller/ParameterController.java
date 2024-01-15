6f00b1d39e2c5e731db030c11656c94418428a4c
lovejr1114 <lovejr1114@naver.com>
2024-01-15 11:25:03 +0900

    한국아니, BMI 계산

--- spring04parameter/src/main/java/com/kh/spring04/controller/ParameterController.java
+++ spring04parameter/src/main/java/com/kh/spring04/controller/ParameterController.java
@@ -1,5 +1,8 @@
 package com.kh.spring04.controller;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +18,22 @@ public class ParameterController {
 		int result = a+b;
 		return "덧셈결과 : "+ result;
 	}
-
+	
+	//(Q) 출생년도 8자리를 입력하면 한국 나이를 반환
+	@RequestMapping("/koreanAge")
+	public String koreanAge(int birth) {
+		int year = birth/10000;
+		int currentAge = LocalDate.now().getYear()-year+1;
+		return "한국나이는 " + currentAge;
+	}
+	
+	
+	//(Q) 키 (cm) 와 체중(kg)을 입력하면 BMI를 반환
+	@RequestMapping("/bmi")
+	public String bmi(float cm, float kg) {
+		float m = cm/100; //float로 시작했으면 끝까지. 중간에 double로 바뀌면 안된다
+		float result = kg / (m*m);
+		DecimalFormat fmt = new DecimalFormat("#,##0.00"); // 소수점 두자리까지
+		return "BMI 지수 = "+ fmt.format(result);
+	}
 }
