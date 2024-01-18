87094331561f83d60ee99110a2b5caedb2918584
lovejr1114 <lovejr1114@naver.com>
2024-01-18 15:58:05 +0900

    pocketmon정보수정 페이지 구현

--- spring10integrated/src/main/java/com/kh/spring10/controller/PocketmonController.java
+++ spring10integrated/src/main/java/com/kh/spring10/controller/PocketmonController.java
@@ -2,7 +2,9 @@ package com.kh.spring10.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.spring10.dao.PocketmonDao;
@@ -34,4 +36,35 @@ public class PocketmonController {
 		return "/WEB-INF/views/pocketmon/insert3.jsp";
 	}
 
+	//수정페이지
+	//- 등록페이지와 매우 유사한 흐름을 가진다
+	//- 입력된 정보 중 기본키를 제외한 나머지 정보를 수정한다
+	//- 수정이 될 수도 안될 수도 있다. 그에 따른 결과 페이지가 달라진다 
+	
+	@GetMapping("/edit")
+	public String edit() {
+		return "/WEB-INF/views/pocketmon/edit.jsp";
+	}
+	
+	@PostMapping("/edit")
+	public String edit(@ModelAttribute PocketmonDto dto) {
+		if(dao.update(dto)){
+//			return "redirect:/pocketmon/editSuccess"; //절대
+			return "redirect:editSuccess"; //상대
+		}
+		else {
+//			return "redirect:/pocketmon/editFail"; //절대
+			return "redirect:editFail"; //상대
+		}
+	}
+	
+	@RequestMapping("/editSuccess")
+	public String editSuccess() {
+		return "/WEB-INF/views/pocketmon/editSuccess.jsp";
+	}
+	
+	@RequestMapping("/editFail")
+	public String editFail() {
+		return "/WEB-INF/views/pocketmon/editFail.jsp";
+	}
 }
---
+++ spring10integrated/src/main/webapp/WEB-INF/views/pocketmon/editFail.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>존재하지 않는 포켓몬스터 번호</h1>
+
+<button><a href="edit">다시하기</a></button>
