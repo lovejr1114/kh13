d7a35360bc0bfac6f203aef7af81908d92a797f7
lovejr1114 <lovejr1114@naver.com>
2024-01-19 10:49:34 +0900

    240119 평가 메모장

---
+++ jdbc03/src/jdbc/dao/GoodsTestDao.java
@@ -0,0 +1,21 @@
+package jdbc.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class GoodsTestDao {
+
+//	public void insert(GoodsDto dto) {
+//		String sql = "insert into goods(no, name, type, price, made) "
+//				+ "values (no_seq.nextval, ?, ?, ?, sysdate, sysdate+30)";
+//		Object[] data = {dto.getName(), dto.getType(), dto.getPrice(), 
+//								dto.getMade(), dto.getExpire()};
+//		JdbcTemplate jdbcTemplate = JdbcUtils.getJdbcTemplate();
+//		jdbcTemplate.update(sql, data);
+//		}
+	
+//	String sql = "insert into board(board_no, board_title, board_content) "
+//				+ "values(board_seq.nextval, ?, ?)";
+//	
+//	Object[] param = {"테스트제목", "테스트내용"};
+//	jdbcTemplate.update(sql, param);
+}
