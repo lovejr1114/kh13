fdccfd1080709d7bd2b5e94b545e0aa5a4bb23d8
lovejr1114 <lovejr1114@naver.com>
2024-02-06 13:10:37 +0900

    포인트 구매내역 출력 구현

--- spring10integrated/src/main/java/com/kh/spring10/dao/BuyDao.java
+++ spring10integrated/src/main/java/com/kh/spring10/dao/BuyDao.java
@@ -1,5 +1,7 @@
 package com.kh.spring10.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -16,7 +18,7 @@ public class BuyDao {
 	@Autowired
 	private BuyMapper buyMapper;
 	
-	
+	//구매내역 등록
 	public void insert(BuyDto buyDto) {
 		String sql = "insert into buy("
 				+ "buy_serial, member_id, item_no, "
@@ -26,5 +28,29 @@ public class BuyDao {
 				buyDto.getMemberId(), buyDto.getItemNo(),
 				buyDto.getItemName(), buyDto.getBuyQty(), buyDto.getBuyTotal() };
 		jdbcTemplate.update(sql, data);
+	}
+	
+	
+	//구매내역을 조회
+	//[1] 자신(특정 회원)의 구매내역을 조회할 수 있는 기능
+	public List<BuyDto> selectList(String memberId){
+		String sql = "select * from buy where member_id = ? "
+						+ "order by buy_serial desc";
+		Object[] data = {memberId};
+		return jdbcTemplate.query(sql, buyMapper, data);
+	}
+	
+	//[2] 관리자가 모든 회원의 구매내역을 조회할 수 있는 기능
+	public List<BuyDto> selectList(int itemNo){
+		String sql = "select * from buy where item_no = ? "
+				+ "order by buy_serial desc";
+		Object[] data = {itemNo};
+		return jdbcTemplate.query(sql, buyMapper, data);
+	}
+	
+	//[3] 관리자가 모든 회원의 구매내역을 조회할 수 있는 기능
+	public List<BuyDto> selectList(){
+		String sql = "select * from buy order by buy_serial desc";
+		return jdbcTemplate.query(sql, buyMapper);
 	}
 }
