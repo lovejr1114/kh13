a2f5687468820b93d1d31f5c98cb292cc25a32e7
lovejr1114 <lovejr1114@naver.com>
2024-02-27 15:14:56 +0900

    회원 닉네임 중복 검사

--- spring11ajax/src/main/java/com/kh/spring11/restcontroller/AjaxRestController.java
+++ spring11ajax/src/main/java/com/kh/spring11/restcontroller/AjaxRestController.java
@@ -2,12 +2,15 @@ package com.kh.spring11.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring11.dao.MemberDao;
+import com.kh.spring11.dao.PocketmonDao;
 import com.kh.spring11.dto.MemberDto;
+import com.kh.spring11.dto.PocketmonDto;
 
 //@Controller // 사용자에게 화면을 전송하는 컨트롤러
 
@@ -17,6 +20,8 @@ public class AjaxRestController {
 	
 	@Autowired
 	private MemberDao memberDao;
+	@Autowired
+	private PocketmonDao pocketmonDao;
 	
 	@RequestMapping("/hello")
 	public String hello() {
@@ -34,6 +39,26 @@ public class AjaxRestController {
 		else {
 			return "NNNNN"; // 사용 불가능한 경우 (DB에 있는 경우)			
 		}
+	}
+
+	
+	@PostMapping("/checkPocketmonNo")
+	public boolean checkPocketmonNo(@RequestParam int pocketmonNo) {
+		PocketmonDto pocketmonDto = pocketmonDao.selectOne(pocketmonNo);
+		return pocketmonDto == null;
+//		if(pocketmonDto == null) {
+//			return true;
+//		}
+//		else {
+//			return false;
+//		}
+	}
+	
+	
+	@PostMapping("/checkMemberNick")
+	public boolean checkMemberNick(@RequestParam String memberNick) {
+		MemberDto memberDto = memberDao.selectOneByMemberNick(memberNick);
+		return memberDto == null;
 	}
 
 }
