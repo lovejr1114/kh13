cea85ff87db781a9204e01f37165c6e57a1cca14
lovejr1114 <lovejr1114@naver.com>
2024-03-11 10:03:50 +0900

    semiProject 생성

---
+++ semiProject/src/main/java/semiProject/Spring12email1Application.java
@@ -0,0 +1,13 @@
+package semiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring12email1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring12email1Application.class, args);
+	}
+
+}
---
+++ semiProject/src/test/java/semiProject/Spring12email1ApplicationTests.java
@@ -0,0 +1,13 @@
+package semiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring12email1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
