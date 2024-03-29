33e8dc88775394e3c8d7ce17040fc60a7451112b
lovejr1114 <lovejr1114@naver.com>
2024-03-29 15:34:59 +0900

    emp 삭제 리절트코드 추가

--- spring14mybatis/src/test/java/com/kh/spring14/EmpDeleteTest01.java
+++ spring14mybatis/src/test/java/com/kh/spring14/EmpDeleteTest01.java
@@ -18,11 +18,13 @@ public class EmpDeleteTest01 {
 	@Test
 	public void test() {
 		int empNo = 8;
-		if(empDao.delete(empNo)) {
-			log.debug("삭제 성공");
-		}
-		else {
-			log.debug("존재하지 않는 사원번호");
-		}
+		boolean result = empDao.delete(empNo);
+		log.debug("result={}",result);
+//		if(empDao.delete(empNo)) {
+//			log.debug("삭제 성공");
+//		}
+//		else {
+//			log.debug("존재하지 않는 사원번호");
+//		}
 	}
 }
