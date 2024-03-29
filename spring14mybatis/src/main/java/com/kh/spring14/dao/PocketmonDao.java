d4fd9c4c384822ddcc9b1ef2f5658f92d671b4c2
lovejr1114 <lovejr1114@naver.com>
2024-03-29 10:35:06 +0900

    spring14mybatis폴더생성 및 dto dao xml파일생성

---
+++ spring14mybatis/src/main/java/com/kh/spring14/dao/PocketmonDao.java
@@ -0,0 +1,25 @@
+package com.kh.spring14.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring14.dto.PocketmonDto;
+
+@Repository
+public class PocketmonDao {
+
+//	@Autowired
+//	private JdbcTemplate jdbcTemplate;
+	//이제 이거 가져올 필요 없다. 이것은 Spring
+	
+	@Autowired
+	private SqlSession sqlSession; //myBatis
+	
+	public List<PocketmonDto> selectList(){
+//		return pocketmon 영역의 list라는 구문을 실행해서 나온 결과;
+		return sqlSession.selectList("pocketmon.list");
+	}
+}
