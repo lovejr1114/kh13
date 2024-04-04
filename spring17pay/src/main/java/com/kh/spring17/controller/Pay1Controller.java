3405a73a4511357d1d48cdd809e834bb5b4b9482
lovejr1114 <lovejr1114@naver.com>
2024-04-04 14:35:43 +0900

    카카오페이 준비요청 페이지 구현

---
+++ spring17pay/src/main/java/com/kh/spring17/controller/Pay1Controller.java
@@ -0,0 +1,38 @@
+package com.kh.spring17.controller;
+
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring17.service.KakaoPayService;
+import com.kh.spring17.vo.KakaoPayApproveResponseVO;
+import com.kh.spring17.vo.KakaoPayReadyRequestVO;
+import com.kh.spring17.vo.KakaoPayReadyResponseVO;
+
+@Controller
+@RequestMapping("/pay1")
+public class Pay1Controller {
+	
+	@Autowired
+	private KakaoPayService kakaoPayService;
+	
+	@GetMapping("/purchase")
+	public String purchase() {
+		return "pay1/purchase";
+	}
+	@PostMapping("/purchase")
+	public String purchase(@ModelAttribute KakaoPayReadyRequestVO requestVO) throws URISyntaxException {
+		requestVO.setPartnerOrderId(UUID.randomUUID().toString());
+		requestVO.setPartnerUserId("testuser1");
+		
+		KakaoPayReadyResponseVO responseVO = kakaoPayService.ready(requestVO);
+		
+		return "redirect:"+responseVO.getNextRedirectPcUrl();
+	}
+}
