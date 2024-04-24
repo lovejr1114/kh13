4d62e9d4b91df292274f3a472ce7e29a6387151f
lovejr1114 <lovejr1114@naver.com>
2024-04-24 10:15:40 +0900

    토큰 추가 및 변수명 변경

--- spring19rest/src/main/java/com/kh/spring19/restcontroller/MemberRestController.java
+++ spring19rest/src/main/java/com/kh/spring19/restcontroller/MemberRestController.java
@@ -32,15 +32,18 @@ public class MemberRestController {
 			return ResponseEntity.notFound().build(); //404
 		}
 		
-		//비밀번호 비교
+		//비밀번호 비교 (암호화가 있다면 코드가 달라질 수 있음)
 		boolean isValid = findDto.getMemberPw().equals(findDto.getMemberPw());
+		
 		if(isValid) { //MemberLoginVO(200) 성공
-			String token = jwtService.create(findDto);
+			String accessToken = jwtService.create(findDto);
+			String refreshToken = jwtService.create(findDto);
 			
 			return ResponseEntity.ok().body(MemberLoginVO.builder()
-						.memberId(findDto.getMemberId())
-						.memberLevel(findDto.getMemberLevel())
-						.token(token)//JWT 토큰
+						.memberId(findDto.getMemberId()) //회원 아이디
+						.memberLevel(findDto.getMemberLevel()) //회원 등급
+						.accessToken(accessToken)//JWT 토큰
+						.refreshToken(refreshToken) //JWT 토큰
 					.build()); //200
 		}
 		else { //실패 - 파라미터가 오류(400)이거나 미인증(401)
