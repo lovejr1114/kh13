755b76efd6e28d4b9338c2bb9a66c1bb8914abd3
lovejr1114 <lovejr1114@naver.com>
2024-04-25 13:33:03 +0900

    페이징 처리한 목록 조회 메소드 구현

--- spring19rest/src/main/java/com/kh/spring19/restcontroller/BookRestController.java
+++ spring19rest/src/main/java/com/kh/spring19/restcontroller/BookRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,15 @@ public class BookRestController {
 	@Autowired
 	private BookDao bookDao;
 	
+	//목록
 	@GetMapping("/")
 	public List<BookDto> list(){
 		return bookDao.selectList();
+	}
+	
+	//페이징
+	@GetMapping("/page/{page}/size/{size}")
+	public List<BookDto> list(@PathVariable int page, @PathVariable int size){
+		return bookDao.selectListByPagin(page, size);
 	}
 }
